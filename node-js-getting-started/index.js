07b4a372fd237f54e63eeb1cf10b47224b3f3e6b
Linus Gordon <linusgordon@gmail.com>
2016-11-16 10:34:58 -0500

    Added changed 2048 file

--- node-js-getting-started/index.js
+++ node-js-getting-started/index.js
@@ -2,6 +2,21 @@ var cool = require('cool-ascii-faces');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var express = require('express');
 var app = express();
+var http = require('http');
+var bodyParser = require('body-parser');
+var validator = require('validator');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoUri = process.env.MONGODB_URI;
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+	if(error) {
+		console.log("Error in connecting: ", error);
+	}
+	db = databaseConnection;
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,11 +33,68 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.set('Content-Type', 'text/html');
+  var indexPage = '';
+  db.collection('scores', function(err, collection) {
+  	collection.find().sort( {score: -1} ).toArray(function(err, cursor) {
+  	if(!err) {
+  		indexPage +="<!DOCTYPE HTML><html><head><title>2048 Game Center</title></head><body><h1>2048 Game Center</h1>";
+  		indexPage +="<table><tr> <th>User</th> <th>Score</th> <th>Timestamp</th></tr>";
+  		for(var i = 0; i < cursor.length; i++) {
+  			indexPage += "<tr><td>" + cursor[i].username + "</td><td>" + cursor[i].score + "</td><td>" + cursor[i].created_at + "</td></tr>";
+  		}
+  		indexPage += "</table></body></html>";
+  		response.send(indexPage);
+  	} else {
+  		indexPage +="<!DOCTYPE HTML><html><head><title>2048 Game Center</title></head><body><h1>Bad</h1></body></html>";
+  		response.send(indexPage);
+  	}
+  });
+  });
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/scores.json', function(request, response) {
+  var indexPage =  "<!DOCTYPE HTML><html><head><title>2048 Game Center</title>";
+  var queryString = request.query.username;
+  indexPage += "<!DOCTYPE HTML><html><head><title>2048 Game Center</title></head><body>";
+  db.collection('scores', function(err, collection) {
+	collection.find({username: queryString}).sort( {score: -1} ).toArray(function(err, cursor) {
+		if(!err) {
+			indexPage += "<body>" + JSON.stringify(cursor) + "</body></html>";
+			response.send(indexPage);
+		}
+	});
+  });
+});
+
+app.post('/submit.json', function(request, response) {
+  var username = request.body.username;
+  var score = request.body.score;
+  var grid = request.body.grid;
+
+  if(username != undefined && score != undefined && grid != undefined) {
+  	var curTime = new Date();
+  	var newEntry = {
+  		"username": username,
+  		"score": score,
+  		"grid": grid,
+  		"created_at": curTime
+  	};
+
+  	db.collection('scores', function(error, call) {
+  		if(error) {
+  			console.log("Error in scores:", error);
+  		}
+  		call.insert(newEntry, function(error, saved) {
+  			if(error) {
+  				console.log("Error in insert for scores: ", error);
+  				response.send(500);
+  			} else {
+  				response.send(200);
+  			}
+  		});
+  	});
+  }
 });
 
 app.get('/redline.json', function(request, response) {
@@ -39,3 +111,4 @@ app.get('/redline.json', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
